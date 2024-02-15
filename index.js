927dfcf6f4a071a4fd4800c39cecc1c4e33f5a41
Shaan E Mustafa <70950454+shaan147@users.noreply.github.com>
2024-02-15 23:51:51 +0500

    installation

--- index.js
+++ index.js
@@ -9,10 +9,27 @@ var bodyParser = require("body-parser");
 const passport = require("passport");
 const localStrategy = require("passport-local");
 const flash = require("connect-flash");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 const app = express();
 const PORT = 3002;
+app.engine("ejs", ejsMate);
+
+app.use(express.static(__dirname + "/public"));
+
+app.set("view engine", "ejs");
+
+app.set(path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+});
+
 app.use(bodyParser.json());
 app.use(express.json());
 // Routes will go here
